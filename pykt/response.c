869b9a2dbfccbb795b63998bd858ace504d615cb
Yutaka Matsubara <yutaka.matsubara@gmail.com>
2011-04-28 18:33:54 +0900

    Fix warn

--- pykt/response.c
+++ pykt/response.c
@@ -1,52 +1,52 @@
 #include "response.h"
 
 
-static inline int
+int
 message_begin_cb(http_parser *p)
 {
     return 0;
 }
 
 static inline int
-header_field_cb (http_parser *p, const char *buf, size_t len, char partial)
+header_field_cb (http_parser *p, const char *buf, size_t len)
 {
     return 0;
 }
 
 static inline int
-header_value_cb (http_parser *p, const char *buf, size_t len, char partial)
+header_value_cb (http_parser *p, const char *buf, size_t len)
 {
     return 0;
 }
 
 static inline int
-request_path_cb (http_parser *p, const char *buf, size_t len, char partial)
+request_path_cb (http_parser *p, const char *buf, size_t len)
 {
 
     return 0;
 }
 
 static inline int
-request_uri_cb (http_parser *p, const char *buf, size_t len, char partial)
+request_url_cb (http_parser *p, const char *buf, size_t len)
 {
     return 0;
 }
 
 static inline int
-query_string_cb (http_parser *p, const char *buf, size_t len, char partial)
+query_string_cb (http_parser *p, const char *buf, size_t len)
 {
     return 0;
 }
 
 static inline int
-fragment_cb (http_parser *p, const char *buf, size_t len, char partial)
+fragment_cb (http_parser *p, const char *buf, size_t len)
 {
     return 0;
 }
 
 
 static inline int
-body_cb (http_parser *p, const char *buf, size_t len, char partial)
+body_cb (http_parser *p, const char *buf, size_t len)
 {
     return 0;
 }
@@ -68,7 +68,7 @@ static http_parser_settings settings =
   ,.on_header_field = header_field_cb
   ,.on_header_value = header_value_cb
   ,.on_path = request_path_cb
-  ,.on_url = request_uri_cb
+  ,.on_url = request_url_cb
   ,.on_fragment = fragment_cb
   ,.on_query_string = query_string_cb
   ,.on_body = body_cb
@@ -93,10 +93,11 @@ init_parser(http_connection *con)
 }
 
 inline size_t 
-execute_parse(http_connection *con, const char *data, size_t len)
+execute_parse(http_connection *con, const char *buf, size_t len)
 {
-
-    return 0;
+    size_t nparsed;
+    nparsed = http_parser_execute(con->parser, &settings, buf, len);
+    return nparsed;
 }
 
 inline int 
--- pykt/response.h
+++ pykt/response.h
@@ -8,7 +8,7 @@ inline http_parser *
 init_parser(http_connection *con);
 
 inline size_t 
-execute_parse(http_connection *con, const char *data, size_t len);
+execute_parse(http_connection *con, const char *buf, size_t len);
 
 inline int 
 parser_finish(http_connection *con);
