cbaf615f77eeabcf312308e84d52f4349bf38d0b
Yutaka Matsubara <yutaka.matsubara@gmail.com>
2011-05-04 07:45:41 +0900

    optimize

--- bench/benchmark.py
+++ bench/benchmark.py
@@ -73,35 +73,43 @@ def kyoto_gets():
         assert ret == val
     db.close()
 
+def pykt_increment():
+    db = kt1()
+    db.open()
+    ret = db.increment("N", 1)
+    db.close()
+
+def kyoto_increment():
+    db = kt2()
+    db.open()
+    ret = db.increment("N", 1)
+    db.close()
+
 if __name__ == "__main__":
-    res = timeit.timeit("pykt_set()", "from __main__ import pykt_set",
-            number=1000)
+    res = timeit.timeit("pykt_set()", "from __main__ import pykt_set", number=1000)
     print "pykt_set %f" % res
-    #res = timeit.timeit("pykt_replace()", "from __main__ import pykt_replace",
-    #        number=1000)
+    #res = timeit.timeit("pykt_replace()", "from __main__ import pykt_replace", number=1000)
     #print "pykt_replace %f" % res
-    #res = timeit.timeit("pykt_append()", "from __main__ import pykt_append",
-    #        number=1000)
-    #print "pykt_append %f" % res
-    res = timeit.timeit("kyoto_set()", "from __main__ import kyoto_set",
-            number=1000)
+    res = timeit.timeit("kyoto_set()", "from __main__ import kyoto_set", number=1000)
     print "kt_set %f" % res
-    #res = timeit.timeit("kyoto_append()", "from __main__ import kyoto_append",
-    #        number=1000)
+    
+    #res = timeit.timeit("pykt_append()", "from __main__ import pykt_append", number=1000)
+    #print "pykt_append %f" % res
+    #res = timeit.timeit("kyoto_append()", "from __main__ import kyoto_append", number=1000)
     #print "kt_append %f" % res
     
-    res = timeit.timeit("pykt_get()", "from __main__ import pykt_get",
-            number=1000)
+    res = timeit.timeit("pykt_get()", "from __main__ import pykt_get", number=1000)
     print "pykt_get %f" % res
-    res = timeit.timeit("kyoto_get()", "from __main__ import kyoto_get",
-            number=1000)
+    res = timeit.timeit("kyoto_get()", "from __main__ import kyoto_get", number=1000)
     print "kt_get %f" % res
 
-    res = timeit.timeit("pykt_gets()", "from __main__ import pykt_gets",
-            number=100)
+    res = timeit.timeit("pykt_gets()", "from __main__ import pykt_gets", number=100)
     print "pykt_gets %f" % res
-    res = timeit.timeit("kyoto_gets()", "from __main__ import kyoto_gets",
-            number=100)
+    res = timeit.timeit("kyoto_gets()", "from __main__ import kyoto_gets", number=100)
     print "kt_gets %f" % res
 
+    res = timeit.timeit("pykt_increment()", "from __main__ import pykt_increment", number=1000)
+    print "pykt_increment %f" % res
+    res = timeit.timeit("kyoto_increment()", "from __main__ import kyoto_increment", number=1000)
+    print "kt_increment %f" % res
 
--- pykt/rpc.c
+++ pykt/rpc.c
@@ -324,6 +324,105 @@ static inline PyObject*
 add_internal(DBObject *db, char *url, size_t url_len, PyObject *keyObj, PyObject *valueObj, PyObject *dbObj, int expire)
 {
     http_connection *con;
+    char *key, *val, *encbuf, *db_name;
+    Py_ssize_t key_len, val_len, db_name_len = 0;
+    char content_length[12];
+    char xt[14];
+    uint64_t expire_time = 0;
+    size_t encbuf_len, xt_len = 0;
+    uint32_t body_len = 12;
+    PyObject *result = NULL, *temp_val;
+
+    DEBUG("rpc_call_add %p %p %p %d", keyObj, valueObj, dbObj, expire);
+    if(!PyString_Check(keyObj)){
+        PyErr_SetString(PyExc_TypeError, "key must be string ");
+        return NULL;
+    }
+    if(dbObj && !PyString_Check(dbObj)){
+        PyErr_SetString(PyExc_TypeError, "db must be string ");
+        return NULL;
+    }
+    if(valueObj == Py_None){
+        PyErr_SetString(PyExc_TypeError, "value is None");
+        return NULL;
+    }
+
+    con = db->con;
+    if(init_bucket(con, 24) < 0){
+        return NULL;
+    }
+    
+    temp_val = PyObject_Str(valueObj);
+    
+    PyString_AsStringAndSize(keyObj, &key, &key_len);
+    PyString_AsStringAndSize(temp_val, &val, &val_len);
+    
+    urlencode(key, key_len, &encbuf, &encbuf_len);
+    body_len += encbuf_len;
+    body_len += val_len;
+
+    if(dbObj){
+        PyString_AsStringAndSize(temp_val, &db_name, &db_name_len);
+        body_len += db_name_len;
+        body_len += 5;
+    }
+    //expire -> expirestr
+    if(expire > 0){
+        expire_time = get_expire_time(expire);
+        //set X-Kt-Kt
+        snprintf(xt, sizeof (xt), "%llu", expire_time);
+        xt_len = strlen(xt);
+        body_len += xt_len;
+        body_len += 5;
+    }
+
+    set_request_path(con, METHOD_POST, LEN(METHOD_POST), url, url_len);
+    snprintf(content_length, sizeof (content_length), "%d", body_len);
+    add_content_length(con, content_length, strlen(content_length));
+    add_header_oneline(con, KT_CONTENT_TYPE, LEN(KT_CONTENT_TYPE));
+    end_header(con);
+    
+    if(db_name_len > 0){
+        add_body(con, "DB\t", 3);
+        add_body(con, db_name, db_name_len);
+        add_body(con, CRLF, 2);
+    }
+
+    add_body(con, "key\t", 4);
+    add_body(con, encbuf, encbuf_len);
+    add_body(con, CRLF, 2);
+    add_body(con, "value\t", 6);
+    add_body(con, val, val_len);
+
+    if(xt_len > 0){
+        add_body(con, CRLF, 2);
+        add_body(con, "xt\t", 3);
+        add_body(con, xt, xt_len);
+    }
+
+    if(request(con, 200) > 0){
+        result = Py_True;
+        Py_INCREF(result);
+    }else{
+        if(con->response_status == RES_SUCCESS){
+            set_error(con);
+        }else{
+            PyErr_SetString(KtException, "could not set error ");
+        }
+    }
+    
+    free_http_data(con);
+    PyMem_Free(encbuf);
+    Py_DECREF(temp_val);
+
+    return result;
+}
+
+/*
+static inline PyObject* 
+add_internal(DBObject *db, char *url, size_t url_len, PyObject *keyObj, PyObject *valueObj, PyObject *dbObj, int expire)
+{
+    http_connection *con;
     char content_length[12];
     buffer *body;
     PyObject *result = NULL;
@@ -387,6 +486,7 @@ add_internal(DBObject *db, char *url, size_t url_len, PyObject *keyObj, PyObject
 
     return result;
 }
+*/
 
 inline PyObject* 
 rpc_call_add(DBObject *db, PyObject *keyObj, PyObject *valueObj, PyObject *dbObj, int expire)
