6f97155d69984028030c80025b5e815267a96c99
Yutaka Matsubara <yutaka.matsubara@gmail.com>
2011-05-06 17:26:53 +0900

    Add regex test

--- tests/test_match_regex.py
+++ tests/test_match_regex.py
@@ -10,50 +10,61 @@ def clear():
     db.close()
 
 @raises(IOError)
-def test_err_prefix():
+def test_err_regex():
     db = KyotoTycoon()
     ret = db.match_prefix("A")
 
 @with_setup(setup=clear)
-def test_match_prefix1():
+def test_match_regex1():
     db = KyotoTycoon()
     db = db.open()
     db.set("ABC", "B")
     db.set("BC", "B")
     db.set("C", "B")
-    ret = db.match_prefix("A")
+    ret = db.match_regex("A")
     ok_(ret == ["ABC"])
     db.close()
 
 @with_setup(setup=clear)
-def test_match_prefix2():
+def test_match_regex2():
     db = KyotoTycoon()
     db = db.open()
     db.set("ABC", "B")
     db.set("AAA", "B")
     db.set("C", "B")
-    ret = db.match_prefix("A")
+    ret = db.match_regex("A")
     ok_(ret == ["AAA", "ABC"])
     db.close()
 
 @with_setup(setup=clear)
-def test_prefix_notfound():
+def test_match_regex3():
+    db = KyotoTycoon()
+    db = db.open()
+    db.set("ABC", "B")
+    db.set("AAA", "B")
+    db.set("C", "B")
+    ret = db.match_regex("A$")
+    ok_(ret == ["AAA"])
+    db.close()
+
+@with_setup(setup=clear)
+def test_notfound():
     db = KyotoTycoon()
     db = db.open()
     db.set("BC", "B")
     db.set("C", "B")
-    ret = db.match_prefix("A")
+    ret = db.match_regex("A")
     ok_(ret == [])
     db.close()
 
 @with_setup(setup=clear)
-def test_match_prefix_utf8():
+def test_match_regex_utf8():
     db = KyotoTycoon()
     db = db.open()
     db.set("あいうえお", "B")
     db.set("BC", "B")
     db.set("C", "B")
-    ret = db.match_prefix("あいう")
+    ret = db.match_regex("あいう")
     ok_(ret == ["あいうえお"])
     db.close()
 
